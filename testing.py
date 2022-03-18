d334291bbcde1554711043df0d85ebd9effb6abe
Marcel Roth <56437044+ximix424@users.noreply.github.com>
2022-03-18 12:09:03 +0100

    feat(MIN-3): adds scaffold of reading eeg data with brainflow library

---
+++ testing.py
@@ -0,0 +1,64 @@
+import argparse
+import time
+
+from brainflow.board_shim import BoardShim, BrainFlowInputParams, BoardIds
+
+
+
+
+def main():
+    BoardShim.enable_dev_board_logger()
+
+    parser = argparse.ArgumentParser()
+    # use docs to check which parameters are required for specific board, e.g. for Cyton - set serial port
+    parser.add_argument('--timeout', type=int, help='timeout for device discovery or connection', required=False,
+                        default=0)
+    parser.add_argument('--ip-port', type=int, help='ip port', required=False, default=0)
+    parser.add_argument('--ip-protocol', type=int, help='ip protocol, check IpProtocolType enum', required=False,
+                        default=0)
+    parser.add_argument('--ip-address', type=str, help='ip address', required=False, default='')
+    parser.add_argument('--serial-port', type=str, help='serial port', required=False, default='COM3')
+    parser.add_argument('--mac-address', type=str, help='mac address', required=False, default='')
+    parser.add_argument('--other-info', type=str, help='other info', required=False, default='')
+    parser.add_argument('--streamer-params', type=str, help='streamer params', required=False, default='')
+    parser.add_argument('--serial-number', type=str, help='serial number', required=False, default='')
+    parser.add_argument('--board-id', type=int, help='board id, check docs to get a list of supported boards',
+                        required=False, default='2')
+    parser.add_argument('--file', type=str, help='file', required=False, default='')
+    args = parser.parse_args()
+
+    params = BrainFlowInputParams()
+    params.ip_port = args.ip_port
+    params.serial_port = args.serial_port
+    params.mac_address = args.mac_address
+    params.other_info = args.other_info
+    params.serial_number = args.serial_number
+    params.ip_address = args.ip_address
+    params.ip_protocol = args.ip_protocol
+    params.timeout = args.timeout
+    params.file = args.file
+
+    board = BoardShim(BoardIds.CYTON_DAISY_BOARD, params)
+    print("Preparing Session")
+    board.prepare_session()
+    print("Successfully prepared Session")
+
+    print("Start streaming data")
+    board.start_stream()
+
+
+
+
+    while True:
+        time.sleep(2)
+        # data = board.get_board_data()  # get all data and remove it from internal buffer
+        print(board.get_board_data(1))
+
+    else:
+        board.stop_stream()
+        print("Stop streaming data")
+        board.release_session()
+
+
+if __name__ == "__main__":
+    main()
