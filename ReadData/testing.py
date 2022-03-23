b7b052dc3c7c3a5b1798139821f098d397b18dbb
ch-wengler <72490062+ch-wengler@users.noreply.github.com>
2022-03-23 14:09:35 +0100

    feat(MIN-10):Create Sliding Windows
    Create sliding windows with variable window size (given by the amount of samples)

--- ReadData/testing.py
+++ ReadData/testing.py
@@ -12,10 +12,34 @@ def read():
     # create a new inlet to read from the stream
     inlet = StreamInlet(streams[0])
 
+    window_size = 125 # size of sliding window in amount of samples
     bool_read = True
     valid_samples = 0
     offset = 12  # in count of samples, *8ms for time
     offset_data = []
+    bool_start = True
+    pushed_elements_count = 0 # counts samples which are pushed into data from one chunk
+    while bool_start:
+        chunk, timestamp = inlet.pull_chunk()
+        pushed_elements_count = 0
+        if chunk:
+            # print(chunk);
+            for sample in chunk:
+                # print(sample)
+                data.append(sample)
+                valid_samples += 1
+                pushed_elements_count += 1
+                if window_size == valid_samples:
+                    valid_samples = 0
+                    bool_start = False
+                break
+        #ToDo call Alghorithmus
+
+    #handle rest of samples in  the chunk
+    for i in range(len(chunk)):
+        if i >= pushed_elements_count:
+            offset_data.append(chunk[i])
+            valid_samples += 1
 
     while bool_read:
         # get chunks of samples
@@ -33,13 +57,19 @@ def read():
                     offset_data.clear()
 
 
+#fill new offset in the sliding window and shift the oldest offset out
+#offset_data array with the eeg data; offset size of the ofset
 def create_sliding_window(offset_data, offset):
-    data.append(offset_data)
+    for i in range(offset):
+        data.append(offset_data[i])
+
+    for i in range(offset):
+        data.pop(0)
+
+    #ToDo call Algorithmus
+
     print(data)
     print(len(data))
-
-
-    data.pop(0)
 
 
 read()
