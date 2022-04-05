8743b2fade712ee96534becd06582e7212fa2897
Pascal Halmer <halmpa01@thu.de>
2022-04-05 15:04:17 +0200

    feat(MIN-48): adds interface to get raw eeg data

---
+++ ReadData/read_data.py
@@ -0,0 +1,83 @@
+import numpy
+import numpy as np
+from numpy_ringbuffer import RingBuffer
+import time
+import serial
+import serial.tools.list_ports
+import copy
+
+import brainflow
+from brainflow.board_shim import BoardShim, BrainFlowInputParams
+
+time_for_one_sample = 0.008  # time which is needed for one sample in s, T = 1/f = 1/125 = 0.008
+bool_stream = False
+board: BoardShim
+buffer = [RingBuffer(capacity=10 * 125, dtype=float) for x in range(16)]
+
+
+def init():
+    params = BrainFlowInputParams()
+    params.serial_port = search_port()
+
+    if params.serial_port is not None:
+        BoardShim.enable_dev_board_logger()
+        global board
+        board = BoardShim(brainflow.board_shim.BoardIds.CYTON_DAISY_BOARD, params)
+        board.prepare_session()
+        board.start_stream()
+        global bool_stream
+        bool_stream = True
+        handle_samples()
+    else:
+        print('Port not found')
+
+
+def search_port():
+    port_name = None
+    print('Search...')
+    ports = serial.tools.list_ports.comports(include_links=False)
+    for port in ports:
+        if port.vid == 1027 and port.pid == 24597:
+            port_name = port.device
+            print('found port: ', port_name)
+            return port_name
+    return None
+
+
+def handle_samples():
+    count = 0
+    while bool_stream:
+        data = board.get_board_data(1)[board.get_eeg_channels(
+            brainflow.board_shim.BoardIds.CYTON_DAISY_BOARD)]  # get all data and remove it from internal buffer
+        if len(data[0]) > 0:
+            for i in range(len(data)):
+                buffer[i].append(data[i][0])
+            count += 1
+            if count == 100:
+                # print(buffer)
+                print(get_trial_data(500000000000000000))
+                stop_stream()
+
+
+def get_trial_data(duration_in_ms: int) -> np.ndarray:
+    copied_buffer = copy.deepcopy(buffer)
+    duration_in_samples = int(duration_in_ms/(time_for_one_sample*1000))
+    i = len(copied_buffer[0]) - duration_in_samples
+    if i < 0:
+        print('not enough data')
+        raise IndexError('Duration to long!')
+    trial_data = [[] for x in range(16)]
+    for x in range(16):
+        trial_data[x] = np.array(copied_buffer[x][i:])
+    return np.array(trial_data)
+
+
+def stop_stream():
+    global bool_stream
+    bool_stream = False
+    time.sleep(1)
+    board.stop_stream()
+    board.release_session()
+
+
+init()
