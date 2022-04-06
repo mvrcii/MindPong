06f1696b0eb8699a1621479438b38e2a8eb04454
Christian1002 <61010485+Christian1002@users.noreply.github.com>
2022-04-06 13:49:02 +0200

    Update BCIC path location

--- algorithms/BCIC_dataset_loader.py
+++ algorithms/BCIC_dataset_loader.py
@@ -1,6 +1,7 @@
 from typing import List
 import mne
 import numpy as np
+import os
 
 CHANNELS = [
             'Fz', 'FC3', 'FC1', 'FCz', 'FC2', 'FC4', 'C5',
@@ -10,8 +11,7 @@ CHANNELS = [
 SAMPLERATE = 250
 TMIN = -2
 TMAX = 5.5
-# path = 'BCIC_dataset/'
-path = '/opt/datasets/BCICompetition_IV-2a/Numpy_files/'
+path = '/BCIC_dataset/'
 NOTCH_FILTER_FREQ: float = 50
 
 
@@ -30,9 +30,9 @@ def calc_n_samples(tmin: float, tmax: float, samplerate: float):
 
 def get_subject_fname(subject: int, training: int = 1):
     if training == 1:
-        return path + 'A0' + str(subject) + 'T.npz'
+        return os.getcwd() + path + 'A0' + str(subject) + 'T.npz'
     elif training == 0:
-        return path + 'A0' + str(subject) + 'E.npz'
+        return os.getcwd() + path + 'A0' + str(subject) + 'E.npz'
     else:
         print('Error: Illegal parameter')
     return None
