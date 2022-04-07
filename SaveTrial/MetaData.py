bf1ad11d224076415aca105af2e24247323fb841
ch-wengler <72490062+ch-wengler@users.noreply.github.com>
2022-04-07 10:37:19 +0200

    refactor(MIN-64): added comments

--- SaveTrial/MetaData.py
+++ SaveTrial/MetaData.py
@@ -1,35 +1,54 @@
 import datetime
 
 
+
 class MetaData:
-    __subject_ID = 1
+
+    """
+    Class to store the meta data of one trial in an object
+    """
+    __subject_ID: int
     __date: datetime.date
     __time: datetime.time
     __amount_samples: int
     __sampling_rate: int
 
-
-    def __init__(self, sid=1, time=datetime.datetime.now().time(), amount_samples=0, sampling_rate=125):
+    def __init__(self, sid, amount_samples, time=datetime.datetime.now().time(), sampling_rate=125):
+        """
+        Date of the session is automatically the current date
+        :param sid: ID of the subject
+        :type sid: int
+        :param amount_samples: amount of the EEG data samples in this trial
+        :type amount_samples: int
+        :param time: start time of the trial
+        :type time: datetime.time
+        :param sampling_rate: sampling rate
+        :type sampling_rate: int
+        """
         self.__subject_ID = sid
         self.__date = datetime.date.today()
         self.__time = time
         self.__amount_samples = amount_samples
         self.__sampling_rate = sampling_rate
 
-    def get_subject_ID(self):
+    @property
+    def subject_ID(self):
         return self.__subject_ID
 
-    def get_date(self):
+    @property
+    def date(self):
         return self.__date
 
-    def get_time(self):
+    @property
+    def time(self):
         return self.__time
 
-    def get_amount_samples(self):
+    @property
+    def amount_samples(self):
         return self.__amount_samples
 
-    def get_sampling_rate(self):
+    @property
+    def sampling_rate(self):
         return self.__sampling_rate
-
 
 
