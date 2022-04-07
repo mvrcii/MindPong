286c5f451f65eb6da0d58c202341f349a168bfd2
Christian1002 <christiandittrich22@gmail.com>
2022-04-07 12:34:04 +0200

    feat(MIN-40): Add label to live plot

--- algorithms/cca_test.py
+++ algorithms/cca_test.py
@@ -8,16 +8,17 @@ import cursor_online_control
 import BCIC_dataset_loader as bdl
 
 # GLOBAL DATA
-TMIN = 700.0        # Minimum time value shown in the following figures
-TMAX = 850.0        # Maximum time value shown in the following figures
-TS_SIZE = 1.0       # 1 s time slice
-TS_STEP = 0.2       # 50 ms
+TMIN = 700.0  # Minimum time value shown in the following figures
+TMAX = 850.0  # Maximum time value shown in the following figures
+TS_SIZE = 1.0  # 1 s time slice
+TS_STEP = 0.2  # 50 ms
 SAMPLING_RATE = 250
-plot_data = list()
-plot_label = list()
 num_used_channels = 0
 mutex = threading.Lock
-q1 = queue.Queue(300)
+QUEUE_DATA = queue.Queue(300)
+QUEUE_LABEL = queue.Queue(300)
+SLIDING_WINDOW_SIZE_FACTOR = 5
+VALID_VALUES_BORDER = 0.3
 '''
 Sliding window size: 
     1 -> 200ms
@@ -25,12 +26,9 @@ Sliding window size:
     ...
     5 -> 1s
 '''
-SLIDING_WINDOW_SIZE_FACTOR = 5
-VALID_VALUES_BORDER = 0.3
 
 
 class MainWindow(QtWidgets.QMainWindow):
-
     def __init__(self, *args, **kwargs):
         super(MainWindow, self).__init__(*args, **kwargs)
 
@@ -38,27 +36,42 @@ class MainWindow(QtWidgets.QMainWindow):
         self.setCentralWidget(self.graphWidget)
 
         self.x = list(range(100))
-        self.y = [0]*100
+        self.data = [0] * 100
+        self.label = [0] * 100
 
-        self.graphWidget.setBackground('w')
+        self.graphWidget.setBackground('k')
+        self.graphWidget.showGrid(x=True, y=True)
 
-        pen = pg.mkPen(width=2, color=(255, 0, 0))
-        self.data_line = self.graphWidget.plot(self.x, self.y, pen=pen)
+        pen_data = pg.mkPen(width=5, color=(255, 0, 255))
+        pen_label = pg.mkPen(width=5, color=(255, 255, 0))
+
+        self.data_line_data = self.graphWidget.plot(self.x, self.data, pen=pen_data)
+        self.data_line_label = self.graphWidget.plot(self.x, self.label, pen=pen_label)
 
         self.timer = QtCore.QTimer()
-        self.timer.setInterval(200)
+        self.timer.setInterval(50)
         self.timer.timeout.connect(self.update_plot_data)
         self.timer.start()
 
     def update_plot_data(self):
+        """
+        Periodically called to update the values by extracting the current values from the queues.
+        """
         try:
+            global QUEUE_DATA, QUEUE_LABEL
+            # update x value
             self.x = self.x[1:]  # Remove the first y element.
             self.x.append(self.x[-1] + 1)  # Add a new value 1 higher than the last.
-            global q1
-            self.y = self.y[1:]
-            self.y.append(q1.get()) # Get and remove the first element of the queue
-            print(q1.get())
-            self.data_line.setData(self.x, self.y)  # Update the data.
+
+            # update data value
+            self.data = self.data[1:]  # Get and remove the first element of the queue
+            self.data.append(QUEUE_DATA.get())
+            self.data_line_data.setData(self.x, self.data)  # Update the data.
+
+            # update label value
+            self.label = self.label[1:]  # Get and remove the first element of the queue
+            self.label.append(QUEUE_LABEL.get())
+            self.data_line_label.setData(self.x, self.label)  # Update the data.
         except:
             time.sleep(0.005)
 
@@ -91,7 +104,7 @@ def loadBCICDataset(ch_weight):
     global num_used_channels
     num_used_channels = len(used_ch_names)
 
-    chan_data, label_data = bdl.get_channel_rawdata(subject=3, n_class=2, ch_names=used_ch_names)
+    chan_data, label_data = bdl.get_channel_rawdata(subject=7, n_class=2, ch_names=used_ch_names)
 
     return chan_data, label_data
 
@@ -118,13 +131,9 @@ def test_algorithm(chan_data, label_data):
         stop_idx = int(((toff[i] + TS_SIZE) * SAMPLING_RATE) - 1)
 
         # calls the one and only cursor control algorithm
-        normalized_hcon = cursor_online_control.perform_algorithm(chan_data[:, start_idx:stop_idx], SLIDING_WINDOW_SIZE_FACTOR)
+        normalized_hcon = cursor_online_control.perform_algorithm(chan_data[:, start_idx:stop_idx],
+                                                                  SLIDING_WINDOW_SIZE_FACTOR)
         norm_hcon[i] = normalized_hcon
-        try:
-            global q1
-            q1.put(normalized_hcon)
-        except:
-            print('Fehler: kann nicht reingeldaden werden')
 
         # converts the returned hcon to the corresponding label
         if normalized_hcon > threshold:
@@ -134,18 +143,23 @@ def test_algorithm(chan_data, label_data):
         else:
             calculated_label = -1
 
-        plot_data.append(calculated_label)
+        try:
+            global QUEUE_DATA, QUEUE_LABEL
+            QUEUE_DATA.put(calculated_label)
+            QUEUE_LABEL.put(label[i])
+        except:
+            print('Fehler: kann nicht reingeldaden werden')
 
         # TODO: compare the calculated label with the predefined label, if same -> increase accuracy
 
     # calculate und plot accuracy
-    accuracy /= num_valid_sliding_windows
-    print(f'Accuracy = {accuracy}')
+    # accuracy /= num_valid_sliding_windows
+    # print(f'Accuracy = {accuracy}')
 
 
 def start_algorithm():
     #                 Fz  FC3  FC1  FCz  FC2  FC4  C5  C3  C1  Cz  C2  C4  C6  CP3  CP1  CPz  CP2  CP4  P1  Pz  P2  POz
-    ch_names_weight = [0,  1,   1,   1,   1,   1,   1,  1,  1,  1,  1,  1,  1,  1,   1,   1,   1,   1,   0,  0,  0,  0]
+    ch_names_weight = [0, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 0, 0, 0, 0]
     preloaded_data, preloaded_labels = loadBCICDataset(ch_names_weight)
     test_algorithm(preloaded_data, preloaded_labels)
 
@@ -153,8 +167,8 @@ def start_algorithm():
 if __name__ == '__main__':
     threading.Thread(target=start_algorithm).start()
 
-
     app = QtWidgets.QApplication(sys.argv)
     w = MainWindow()
+    w.showFullScreen()
     w.show()
     sys.exit(app.exec_())
