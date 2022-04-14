0a6b5f8caeeb91581d7cd9aa163f56802937046a
Marcel Roth <56437044+ximix424@users.noreply.github.com>
2022-04-14 10:43:43 +0200

    refactor(MIN-69): refactors app module

--- mvc/app.py
+++ mvc/app.py
@@ -1,8 +1,8 @@
 import tkinter as tk
 from tkinter import ttk
 
-from mvc.gui.controllers import Controller, ConfigController
-from mvc.gui.view import Config
+from mvc.gui.controllers import ConfigController, EEGController
+from mvc.gui.view import ConfigView, EEGView
 
 
 class Application(tk.Frame):
@@ -11,18 +11,35 @@ class Application(tk.Frame):
         self.master = master
         self.columnconfigure(0, weight=1)
         self.rowconfigure(0, weight=1)
-        self.grid(row=0, column=0, sticky=tk.N + tk.S + tk.E + tk.W)
+        self.grid(row=0, column=0, sticky='nsew')
+
+    def create_second_window(self):
+        second_window = tk.Toplevel(master=self.master)
+        second_window.title("MindPong")
+        second_window.geometry("200x200")
+        ttk.Label(second_window, text="This is a new window").pack()
+
+        eeg_controller = EEGController()  # Create Controller
+        eeg_view = EEGView(second_window)  # Create View
+        eeg_controller.bind(eeg_view)  # Bind View to Controller
 
 
 if __name__ == "__main__":
     root = tk.Tk()
+    root.title("Settings")
+
+    # Set the theme initially to light mode
+    root.call("source", "azure.tcl")
+    root.call("set_theme", "light")
+
     root.columnconfigure(0, weight=1)
     root.rowconfigure(0, weight=1)
+    root.resizable(False, False)
 
     app = Application(master=root)
 
-    config_controller = ConfigController()
-    config_view = Config(root)
+    config_controller = ConfigController(app)
+    config_view = ConfigView(root)
     config_controller.bind(config_view)
 
     app.mainloop()
