73831f97204a3e9a00a7749b861d820c3418f951
Marcel Roth <56437044+ximix424@users.noreply.github.com>
2022-04-14 10:44:14 +0200

    refactor(MIN-69): restructures the view

--- mvc/gui/view.py
+++ mvc/gui/view.py
@@ -1,6 +1,7 @@
+import tkinter
 import tkinter as tk
+import tkinter.ttk as ttk
 from abc import abstractmethod
-from tkinter import ttk
 
 
 class View(tk.Frame):
@@ -9,20 +10,17 @@ class View(tk.Frame):
         raise NotImplementedError
 
 
-class Config(View):
-    def __init__(self, master=None):
+class ConfigView(View):
+    def __init__(self, master: tkinter.Tk = None):
         super().__init__(master)
-        self.master = master
+        self.master: tkinter.Tk = master
+        self.spin_boxes = {}
         self.sliders = {}
         self.entries = {}
+        self.labels = {}
         self.buttons = {}
-        self.check_boxes = {}
         self.combo_boxes = {}
-        self.rowconfigure(0, weight=1)
-        self.rowconfigure(1, weight=1)
-        self.rowconfigure(2, weight=1)
-        self.columnconfigure(0, weight=1)
-        self.columnconfigure(1, weight=1)
+        self.switches = {}
         self.grid(row=0, column=0, sticky='nsew')
 
     def create_view(self):
@@ -31,81 +29,90 @@ class Config(View):
         control_frame.columnconfigure(0, weight=1)
         control_frame.grid(row=0, column=0, sticky='nsew')
 
-        self.create_subject(control_frame, "Subject", row=0, column=0)
+        self.build_subject_section(control_frame, "Subject", row=0, column=0)
+        self.build_graph_section(control_frame, "Graph", row=0, column=1)
+        self.build_algorithm_section(control_frame, "Algorithm", row=1, column=0)
+        self.build_window_section(control_frame, "Window", row=2, column=0)
+        self.create_button(control_frame, "Start", row=3, column=0)
 
-        self.create_algorithm(control_frame, "Algorithm", row=1, column=0)
+        # Switch for dark/light mode
+        self.create_switch(control_frame, "Dark-Mode", row=3, column=1, variable=tk.BooleanVar(),
+                           command=self.toggle_dark_mode)
 
-        self.create_slider(control_frame, "Window Size", row=4, column=0, from_=200, to=2000, interval=200,
-                           orientation="horizontal")
-        self.create_slider(control_frame, "Window Offset", row=5, column=0, from_=40, to=400, interval=40,
-                           orientation="horizontal")
-        self.create_slider(control_frame, "Trial Min-Duration", row=6, column=0, from_=800, to=1500, interval=100,
-                           orientation="horizontal")
+    def build_graph_section(self, frame, label, row, column):
+        graph_frame = ttk.LabelFrame(frame, text=label)
+        self.create_switch(graph_frame, "C3", row=0, column=0, variable=tk.BooleanVar(), command=None)
+        self.create_switch(graph_frame, "C4", row=1, column=0, variable=tk.BooleanVar(), command=None)
+        graph_frame.grid(padx=10, pady=5, row=row, column=column, sticky='nsew')
 
-        self.create_button(control_frame, "Start", row=7, column=0)
-
-        # Graph Frame
-        graph_frame = tk.LabelFrame(master=self, text="Graph")
-        graph_frame.columnconfigure(0, weight=1)
-        graph_frame.grid(padx=10, pady=5, row=0, column=1, sticky='nsew')
-
-        self.create_checkbox(graph_frame, "C3", row=0, column=0, variable=tk.BooleanVar())
-        self.create_checkbox(graph_frame, "C4", row=1, column=0, variable=tk.BooleanVar())
-
-    def create_slider(self, frame, label, row, column, from_, to, interval, orientation):
-        label_frame = tk.LabelFrame(frame, text=label)
-        self.sliders[label] = tk.Scale(frame, from_=from_, to=to, tickinterval=interval, orient=orientation,
-                                       resolution=interval)
-        self.sliders[label].grid(padx=20, pady=20, row=row, column=column, sticky='ew')
-        frame.rowconfigure(index=row, minsize=70)
-        frame.columnconfigure(index=column, minsize=400)
+    def build_subject_section(self, frame, label, row, column):
+        label_frame = ttk.LabelFrame(frame, text=label)
+        self.create_entry(label_frame, "ID", row=0, column=0, text_var=tk.IntVar(value=1))
+        self.create_entry(label_frame, "Age", row=0, column=1, text_var=None)
+        self.create_combobox(label_frame, "Sex", values=[], row=0, column=2)
         label_frame.grid(padx=10, pady=5, row=row, column=column, sticky='nsew')
 
-    def create_subject(self, frame, label, row, column):
-        label_frame = tk.LabelFrame(frame, text=label)
-        self.create_entry(label_frame, "ID", row=0, column=0, text_var=tk.IntVar())
-        self.create_combobox(label_frame, "Sex", ["M", "F", "D"], row=0, column=1)
-        self.create_entry(label_frame, "Age", row=0, column=2, text_var=tk.IntVar())
-        label_frame.grid(padx=10, pady=5, row=row, column=column, sticky='nsew')
-
-    def create_algorithm(self, frame, label, row, column):
-        label_frame = tk.LabelFrame(frame, text=label)
+    def build_algorithm_section(self, frame, label, row, column):
+        label_frame = ttk.LabelFrame(frame, text=label)
         self.create_entry(label_frame, "Threshold", row=0, column=0, text_var=tk.DoubleVar())
-        self.create_entry(label_frame, "fmin", row=0, column=1, text_var=tk.DoubleVar())
-        self.create_entry(label_frame, "fmax", row=0, column=2, text_var=tk.DoubleVar())
+        self.create_entry(label_frame, "f_min", row=0, column=1, text_var=tk.DoubleVar())
+        self.create_entry(label_frame, "f_max", row=0, column=2, text_var=tk.DoubleVar())
         label_frame.grid(padx=10, pady=5, row=row, column=column, sticky='nsew')
 
+    def build_window_section(self, frame, label, row, column):
+        spinbox_frame = ttk.LabelFrame(frame, text=label)
+        self.create_spinbox(spinbox_frame, "window_size", "Window Size", row=0, column=0, from_=200, to=2000, interval=200)
+        self.create_spinbox(spinbox_frame, "window_offset", "Window Offset", row=0, column=1, from_=40, to=400, interval=40)
+        self.create_spinbox(spinbox_frame, "trial_min_duration", "Trial Min-Duration", row=0, column=2, from_=800, to=1500, interval=100)
+        spinbox_frame.grid(padx=10, pady=5, row=row, column=column, sticky='nsew')
+
+    # Helper functions
     def create_entry(self, frame, label, row, column, text_var):
-        label_frame = tk.LabelFrame(frame, text=label)
-        self.entries[label] = tk.Entry(label_frame, textvariable=text_var)
-        self.entries[label].grid(row=1, column=1, sticky='ew')
+        self.labels[label] = ttk.Label(text=label)
+        label_frame = tk.LabelFrame(frame, labelwidget=self.labels[label], bd=0)
+        self.entries[label] = ttk.Entry(label_frame, textvariable=text_var)
+        self.entries[label].grid(padx=5, pady=5, row=1, column=1, sticky='nsew')
         self.entries[label].pack(padx=5, pady=5, expand=1, fill='x')
         label_frame.grid(padx=10, pady=5, row=row, column=column, sticky='nsew')
 
     def create_button(self, frame, name, row, column):
-        self.buttons[name] = tk.Button(frame, width=25, relief=tk.RAISED)
-        self.buttons[name]["text"] = name
-        self.buttons[name].grid(padx=10, pady=5, row=row, column=column)
+        self.buttons[name] = ttk.Button(frame, text=name)
+        self.buttons[name].grid(padx=10, pady=5, row=row, column=column, sticky='nsew')
 
     def create_combobox(self, frame, label, values, row, column):
-        label_frame = tk.LabelFrame(frame, text=label)
-        self.combo_boxes[label] = ttk.Combobox(label_frame, values=values)
-        self.combo_boxes[label].grid(row=1, column=1)
-        self.combo_boxes[label].pack(padx=5, pady=5, expand=1, fill='x')
+        self.labels[label] = ttk.Label(text=label)
+        label_frame = tk.LabelFrame(frame, labelwidget=self.labels[label], bd=0)
+        self.combo_boxes[label] = ttk.Combobox(label_frame, state="readonly", values=values)
+        self.combo_boxes[label].grid(row=0, column=0, padx=5, pady=5, sticky='ew')
         label_frame.grid(padx=10, pady=5, row=row, column=column, sticky='nsew')
 
-    def create_checkbox(self, frame, label, variable, row, column):
-        self.check_boxes[label] = ttk.Checkbutton(frame, text=label, variable=variable)
-        self.check_boxes[label].grid(padx=10, pady=5, row=row, column=column, sticky='nsew')
+    def create_switch(self, frame, label, variable, row, column, command):
+        self.switches[label] = variable
+        check_button = ttk.Checkbutton(frame, text=label, style='Switch.TCheckbutton', variable=variable,
+                                       onvalue=True, offvalue=False, command=command)
+        check_button.grid(padx=10, pady=5, row=row, column=column, sticky='nsew')
+
+    def create_spinbox(self, frame, label, text, row, column, from_, to, interval):
+        self.labels[label] = ttk.Label(text=text)
+        label_frame = tk.LabelFrame(frame, labelwidget=self.labels[label], bd=0)
+        self.spin_boxes[label] = ttk.Spinbox(label_frame, state="readonly", from_=from_, to=to, increment=interval)
+        self.spin_boxes[label].grid(row=0, column=0, padx=5, pady=5, sticky="ew")
+        label_frame.grid(padx=10, pady=5, row=row, column=column, sticky='nsew')
+
+    def toggle_dark_mode(self):
+        if self.switches["Dark-Mode"].get():
+            self.master.call("set_theme", "dark")
+        else:
+            self.master.call("set_theme", "light")
 
 
-class EEG(View):
+class EEGView(View):
     def __init__(self, master=None):
         super().__init__(master)
         self.master = master
         self.rowconfigure(0, weight=1)
         self.columnconfigure(0, weight=1)
-        self.grid(row=0, column=0, sticky='nsew')
+        # self.grid(row=0, column=0, sticky='nsew')
 
     def create_view(self):
         control_frame = tk.Frame(master=self)
