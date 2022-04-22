b2d8e364dff69f2db08f2ad91ee0cd44efd5a35b
ch-wengler <72490062+ch-wengler@users.noreply.github.com>
2022-04-22 15:58:44 +0200

    test(MIN-113): Added counter to unit test

--- tests/data/extraction/test_trial_handler.py
+++ tests/data/extraction/test_trial_handler.py
@@ -5,7 +5,7 @@ import unittest
 import numpy as np
 from numpy import dtype
 
-from scripts.data.extraction import trial_handler, MetaData, Labels
+from scripts.data.extraction import trial_handler, MetaData
 
 
 class MyTestCase(unittest.TestCase):
@@ -34,10 +34,18 @@ class MyTestCase(unittest.TestCase):
         data1 = [[1] for _ in range(16)]
         start = time.time()
         trial_handler.send_raw_data(data1, start=start)
-        trial_handler.mark_trial(start + 0.008, start + (0.008 * 5), Labels.LEFT)
+        trial_handler.mark_trial(start + 0.008, start + (0.008 * 5), trial_handler.Labels.LEFT)
         self.assertEqual(1, trial_handler.event_pos[0])
         self.assertEqual(4, trial_handler.event_duration[0])
-        self.assertEqual(Labels.LEFT, trial_handler.event_type[0])
+        self.assertEqual(trial_handler.Labels.LEFT, trial_handler.event_type[0])
+        self.assertEqual(1, trial_handler.count_event_types)
+        self.assertEqual(1, trial_handler.count_trials)
+        data2 = [[2] for _ in range(16)]
+        start = time.time()
+        trial_handler.send_raw_data(data2)
+        trial_handler.mark_trial(start, start + (0.008 * 5), trial_handler.Labels.LEFT)
+        self.assertEqual(2, trial_handler.count_trials)
+        self.assertEqual(1, trial_handler.count_event_types)
 
     def test_save_session(self):
         data1 = [[1] for _ in range(16)]
@@ -49,7 +57,7 @@ class MyTestCase(unittest.TestCase):
         trial_handler.send_raw_data(data2)
         trial_handler.send_raw_data(data3)
         trial_handler.send_raw_data(data4)
-        trial_handler.mark_trial(start + 0.008, start + (0.008 * 5), Labels.LEFT)
+        trial_handler.mark_trial(start + 0.008, start + (0.008 * 5), trial_handler.Labels.LEFT)
         expected_array = [[] for _ in range(16)]
         for i in range(len(expected_array)):
             expected_array[i] = [1, 2, 3, 4]
@@ -66,7 +74,7 @@ class MyTestCase(unittest.TestCase):
         expected_metadata = np.array(meta, dtype=dtype)
         expected_pos = np.array([1])
         expected_duration = np.array([4])
-        expected_type = np.array([Labels.LEFT])
+        expected_type = np.array([trial_handler.Labels.LEFT])
         trial_handler.save_session(metadata, 'test.npz')
         test = np.load('../../../scripts/data/session/test.npz', allow_pickle=True)
         self.assertEqual(test['meta'].all(), expected_metadata.all())
