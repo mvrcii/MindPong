45698c0599b14f32d9925e7c9ba059af2fecefb2
Pascal Halmer <92431663+Pangel33@users.noreply.github.com>
2022-04-22 10:44:38 +0200

    Update BCIC_dataset_loader.py

--- algorithms/BCIC_dataset_loader.py
+++ algorithms/BCIC_dataset_loader.py
@@ -6,10 +6,10 @@ import numpy as np
 from pathlib import Path
 
 CHANNELS = [
-    'Fz', 'FC3', 'FC1', 'FCz', 'FC2', 'FC4', 'C5',
-    'C3', 'C1', 'Cz', 'C2', 'C4', 'C6', 'CP3',
-    'CP1', 'CPz', 'CP2', 'CP4', 'P1', 'Pz', 'P2',
-    'POz']
+            'Fz', 'FC3', 'FC1', 'FCz', 'FC2', 'FC4', 'C5',
+            'C3', 'C1', 'Cz', 'C2', 'C4', 'C6', 'CP3',
+            'CP1', 'CPz', 'CP2', 'CP4', 'P1', 'Pz', 'P2',
+            'POz']
 SAMPLERATE = 250
 TMIN = -2
 TMAX = 5.5
