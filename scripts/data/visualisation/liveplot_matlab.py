1ea2e8f96c87e9caf1ce7c923e909e3b1e9d1220
Christian1002 <christiandittrich22@gmail.com>
2022-04-24 00:05:43 +0200

    refactor(MIN-111): Dynamic queue adding

--- scripts/data/visualisation/liveplot_matlab.py
+++ scripts/data/visualisation/liveplot_matlab.py
@@ -9,87 +9,93 @@ import numpy as np
 plt.style.use('ggplot')
 # necessary!!! to make sure the backend ist the correct one
 matplotlib.use('TkAgg')
-queues = []
-line1 = []
+queues = list()
 size = 100
-x_label = list(range(size))
-q = queue.Queue(size)
-x_vec = np.linspace(0, 100, size + 1)[0:-1]  # scaling of x-axis
-y_vec = np.zeros(size)
+x_data = list(range(100))
 fig = plt.figure(tight_layout=True)
+ax = None
+q1 = queue.Queue(100)
+q2 = queue.Queue(100)
+
+
+class PlotData:
+    def __init__(self, q: queue.Queue):
+        self.q = q
+        self.y_data = np.zeros(size)
+        print(type(ax))
+        self.line, = ax.plot(x_data, self.y_data)
+
+
+def live_plotter(plot_data: PlotData, x_data):
+    # after the figure, axis, and line are created, we only need to update the y-data
+    plot_data.line.set_ydata(plot_data.y_data)
+    # adjust limits if new data goes beyond bounds
+    if np.min(plot_data.y_data) <= plot_data.line.axes.get_ylim()[0] or np.max(plot_data.y_data) >= plot_data.line.axes.get_ylim()[1]:
+        plt.ylim([np.min(plot_data.y_data) - np.std(plot_data.y_data), np.max(plot_data.y_data) + np.std(plot_data.y_data)])
+
+    # ascending x-values only the label a changes the x-range remains the same
+    # TODO: listen to the warning and Use a FixedLocator
+    plot_data.line.axes.set_xticklabels(x_data)
+    # return line, so we can update it again in the next iteration
+    return plot_data.line
+
+
+def do_live_plot():
+    global x_data, fig, queues
+    while True:
+        # necessary if you want to get out of the endless loop after the figure is closed
+        if not plt.get_fignums():
+            break
+        try:
+            x_data = x_data[1:]  # Remove the first y element.
+            x_data.append(x_data[-1] + 1)
+            for plot_data in queues:
+                new_val = plot_data.q.get_nowait()
+                plot_data.y_data[-1] = new_val
+                plot_data.line = live_plotter(plot_data, x_data)
+                plot_data.y_data = np.append(plot_data.y_data[1:], 0.0)
+            plt.pause(0.1)
+        except queue.Empty:
+            time.sleep(0.01)
+
+
+def connect_queue(toggle_plot, queue: queue.Queue):
+    while not ax:
+        time.sleep(0.1)
+    queues.append(PlotData(queue))
+
+
+def start_live_plot(identifier='Signal Plot'):
+    global fig, ax
+    # this is the call to matplotlib that allows dynamic plotting
+    plt.ion()
+    ax = fig.add_subplot(111)
+    # update plot label/title
+    plt.ylabel('Time')
+    plt.title('{}'.format(identifier))
+    # to maximize figure, but it shows a strange behavior
+    # mng = plt.get_current_fig_manager()
+    # mng.resize(*mng.window.maxsize())
+    plt.show()
+    # start liveplot
+    do_live_plot()
 
 
 # fill queue for testing purposes with random values
-def create_values():
+def handle_queue():
+    global q1, q2
+    connect_queue(None, q1)
+    connect_queue(None, q2)
     while True:
         rand_val = np.random.randn(1)
         try:
-            q.put(rand_val)
+            q1.put(rand_val)
+            q2.put(rand_val + 2)
             # print('Alive')
         except queue.Empty:
             time.sleep(0.005)
 
 
-def live_plotter(identifier='', pause_time=0.1):
-    global x_vec, y_vec, line1, x_label
-    y1_data = y_vec
-
-    if not line1:
-        global fig
-        # this is the call to matplotlib that allows dynamic plotting
-        plt.ion()
-        ax = fig.add_subplot(111)
-        # create a variable for the line, so we can later update it
-        line1, = ax.plot(x_vec, y1_data, '-o', alpha=0.8)
-        # update plot label/title
-        plt.ylabel('Time')
-        plt.title('Signal Plot'.format(identifier))
-        # to maximize figure, but it shows a strange behavior
-        # mng = plt.get_current_fig_manager()
-        # mng.resize(*mng.window.maxsize())
-        plt.show()
-
-    # after the figure, axis, and line are created, we only need to update the y-data
-    line1.set_ydata(y1_data)
-    # adjust limits if new data goes beyond bounds
-    if np.min(y1_data) <= line1.axes.get_ylim()[0] or np.max(y1_data) >= line1.axes.get_ylim()[1]:
-        plt.ylim([np.min(y1_data) - np.std(y1_data), np.max(y1_data) + np.std(y1_data)])
-
-    # ascending x-values only the label a changes the x-range remains the same
-    x_label = x_label[1:]  # Remove the first y element.
-    x_label.append(x_label[-1] + 1)
-    # TODO: listen to the warning and Use a FixedLocator
-    line1.axes.set_xticklabels(x_label)
-
-    # this pauses the data so the figure/axis can catch up - the amount of pause can be altered above
-    plt.pause(pause_time)
-    # return line so we can update it again in the next iteration
-    return line1
-
-
-def do_live_plot():
-    global y_vec, x_vec, line1, fig, q
-    if q:
-        while True:
-            # necessary if you want to get out of the endless loop after the figure is closed
-            if not plt.get_fignums():
-                break
-            try:
-                rand_val = q.get_nowait()
-                y_vec[-1] = rand_val
-                line1 = live_plotter()
-                y_vec = np.append(y_vec[1:], 0.0)
-
-            except queue.Empty:
-                time.sleep(0.01)
-
-
-def connect_queue(queue: queue.Queue):
-    global q
-    q = queue
-    do_live_plot()
-
-
 if __name__ == '__main__':
-    threading.Thread(target=create_values, daemon=True).start()
-    do_live_plot()
+    threading.Thread(target=handle_queue, daemon=True).start()
+    start_live_plot()
