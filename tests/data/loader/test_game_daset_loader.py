5d251e87231b914a3b2cd71f9e9591a29befa036
Pascal Halmer <halmpa01@thu.de>
2022-05-05 12:34:52 +0200

    refactor: tests anpassung

--- tests/data/loader/test_game_daset_loader.py
+++ tests/data/loader/test_game_daset_loader.py
@@ -7,8 +7,11 @@ from scripts.data.loader import game_dataset_loader
 class MyTestCase(unittest.TestCase):
 
     def test_get_channel_rawdata(self):
-        chan_data, chan_label = game_dataset_loader.get_channel_rawdata('../../../scripts/data/session/test_loader.npz')
 
+        game_dataset_loader.NOTCH_FILTER = True
+        game_dataset_loader.NOTCH_FILTER_FREQ = 50.0
+
+        chan_data, chan_label = game_dataset_loader.get_channel_rawdata('../../../scripts/data/session/test_loader.npz')
         expected_chan_data = np.array([[1.0369846, 1.9401181, 3.05990587, 3.96296898],
                                        [1.0369846, 1.9401181, 3.05990587, 3.96296898],
                                        [1.0369846, 1.9401181, 3.05990587, 3.96296898],
@@ -26,15 +29,23 @@ class MyTestCase(unittest.TestCase):
                                        [1.0369846, 1.9401181, 3.05990587, 3.96296898],
                                        [1.0369846, 1.9401181, 3.05990587, 3.96296898]])
         expected_chan_label = np.array([-1, 0, 0, 0, 0, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1])
-        self.assertEqual(expected_chan_data.all(), chan_data.all())
-        self.assertEqual(expected_chan_label.all(), chan_label.all())
+
+        for a, b in zip(expected_chan_data, chan_data):
+            for c, d in zip(a, b):
+                self.assertAlmostEqual(q, d, 2)
+        for a, b in zip(expected_chan_label, chan_label):
+            self.assertAlmostEqual(a, b, 2)
 
         chan_data, chan_label = game_dataset_loader.get_channel_rawdata('../../../scripts/data/session/test_loader.npz',
                                                                         ['C4', 'C3'])
         expected_chan_data = np.array([[1.0369846, 1.9401181, 3.05990587, 3.96296898],
                                        [1.0369846, 1.9401181, 3.05990587, 3.96296898]])
-        self.assertEqual(expected_chan_data.all(), chan_data.all())
-        self.assertEqual(expected_chan_label.all(), chan_label.all())
+
+        for a, b in zip(expected_chan_data, chan_data):
+            for c, d in zip(a, b):
+                self.assertAlmostEqual(c, d, 2)
+        for a, b in zip(expected_chan_label, chan_label):
+            self.assertAlmostEqual(a, b, 2)
 
         chan_data, chan_label = game_dataset_loader.get_channel_rawdata('../../../scripts/data/session/test_loader.npz',
                                                                         ['C4', 'C3', 'not a channel name'])
